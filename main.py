26732b9c18ae29a8ee91dccf30f3aa1298d345ad
Furkan Taha Bademci <95905794+FurkanTahaBademci@users.noreply.github.com>
2023-11-01 20:56:02 +0300

    output images change

--- main.py
+++ main.py
@@ -33,29 +33,31 @@ def main():
         shadowy_images= am.add_shadow(image,no_of_shadows=2, shadow_dimension=8)
         speedy_images= am.add_speed(image)
         snowy_images= am.add_snow(image, snow_coeff=0.3)
-
-        process_image(output, img, copy_txt, "snowy", snowy_images, image_type, time_path)
         rainy_images= am.add_rain(image, rain_type='heavy', slant=20)
-        process_image(output, img, copy_txt, "rainy", rainy_images, image_type, time_path)
-        process_image(output, img, False, "bright", bright_image, image_type, time_path)
-        process_image(output, img, False, "dark", dark_image, image_type, time_path)
-        process_image(output, img, False, "shadowy", shadowy_images, image_type, time_path)
-        process_image(output, img, False, "speedy", speedy_images, image_type, time_path)
+
+        process_image(output, img, copy_txt, "snowy", snowy_images, image_type)
+        process_image(output, img, copy_txt, "rainy", rainy_images, image_type)
+        process_image(output, img, False, "bright", bright_image, image_type)
+        process_image(output, img, False, "dark", dark_image, image_type)
+        process_image(output, img, False, "shadowy", shadowy_images, image_type)
+        process_image(output, img, False, "speedy", speedy_images, image_type)
+
 
 def create_directories(output_path, *directories):
     for directory in directories:
         os.makedirs(os.path.join(output_path, directory), exist_ok=True)
 
-def process_image(output, img, copy, folder, processed_image, image_type, time_path):
+def process_image(output, img, copy, folder, processed_image, image_type):
     file_name = os.path.basename(img)
     file_name_without_extension = os.path.splitext(file_name)[0]
-    image_name = f"{file_name_without_extension}_{folder}_{time.time()}.{image_type}"
+    image_name = f"{file_name_without_extension}_{folder}.{image_type}"
     if copy:
         txt_name = file_name.replace(f".{image_type}", ".txt")
         txt_path_new = os.path.join(output, folder, txt_name)
         txt_path_before = img.replace(f".{image_type}", ".txt")
         shutil.copy(txt_path_before, txt_path_new)
     cv2.imwrite(os.path.join(output, folder, image_name), processed_image)
+
     
 if __name__ == "__main__":
     main()

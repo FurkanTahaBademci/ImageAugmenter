e7c7288a9c558695ffb7e802e4cd3a800f909324
Furkan Taha Bademci <95905794+FurkanTahaBademci@users.noreply.github.com>
2023-11-01 20:45:49 +0300

    output add

--- main.py
+++ main.py
@@ -8,56 +8,56 @@ import time
 import shutil
 
 parser = argparse.ArgumentParser(description='Image Augmentation Script')
-parser.add_argument('--input_path', type=str, help='Path to input images directory', required=True)
-parser.add_argument('--output_path', type=str, help='Path to output directory', required=True)
-parser.add_argument('--image_type', type=str, help='Image type (e.g., jpeg, png)', default='jpeg')
+parser.add_argument('--input', type=str, help='Path to input images directory', required=True)
+parser.add_argument('--output', type=str, help='Path to output directory', required=True)
+parser.add_argument('--type', type=str, help='Image type (e.g., jpeg, png)', required=True)
+parser.add_argument('--copy_txt', action='store_true', help='Copy txt files if True, otherwise do not copy')
 
 args = parser.parse_args()
 
-path = args.input_path
-output = args.output_path
-type = args.image_type
+path = args.input
+output = args.output
+type = args.type
+copy_txt = args.copy_txt
 
 image_paths= glob.glob(path+"*."+type)
 time_path = time.time()
 
-os.mkdir(output+f"bright_{time_path}/")
-os.mkdir(output+f"dark_{time_path}/")
-os.mkdir(output+f"shadowy_{time_path}/")
-os.mkdir(output+f"snowy_{time_path}/")
-os.mkdir(output+f"rainy_{time_path}/")
-os.mkdir(output+f"speedy_{time_path}/")
+os.mkdir(output+f"bright/")
+os.mkdir(output+f"dark/")
+os.mkdir(output+f"shadowy/")
+os.mkdir(output+f"snowy/")
+os.mkdir(output+f"rainy/")
+os.mkdir(output+f"speedy/")
 
 for img in tqdm(image_paths):
-    
+
     image = cv2.imread(img)
     bright_image = am.brighten(image)
     dark_image = am.darken(image, darkness_coeff=0.3)
     shadowy_images= am.add_shadow(image,no_of_shadows=2, shadow_dimension=8)
-   
-    speedy_images= am.add_speed(image)
-    
-    snowy_images= am.add_snow(image, snow_coeff=0.3)
-    image_name = f"data_augmente_{time.time()}.jpeg"
-    txt_name = image_name.replace(".jpeg",".txt")
-    txt_path_new = output+f"snowy_{time_path}/{txt_name}"
-    txt_path_before = img.replace(".jpeg",".txt") 
-    shutil.copy(txt_path_before,txt_path_new)
-    cv2.imwrite(output+f"snowy_{time_path}/{image_name}",snowy_images)
-    
-    rainy_images= am.add_rain(image, rain_type='heavy', slant=20)
-    image_name = f"data_augmente_{time.time()}.jpeg"
-    txt_name = image_name.replace(".jpeg",".txt")
-    txt_path_new = output+f"rainy_{time_path}/{txt_name}"
-    txt_path_before = img.replace(".jpeg",".txt") 
-    shutil.copy(txt_path_before,txt_path_new)
-    cv2.imwrite(output+f"rainy_{time_path}/{image_name}",rainy_images)
-    
-    cv2.imwrite(output+f"bright_{time_path}/{image_name}",bright_image);image_name = f"data_augmente_{time.time()}.jpg"
-    cv2.imwrite(output+f"dark_{time_path}/{image_name}",dark_image);image_name = f"data_augmente_{time.time()}.jpg"
-    cv2.imwrite(output+f"shadowy_{time_path}/{image_name}",shadowy_images);image_name = f"data_augmente_{time.time()}.jpg"
-    cv2.imwrite(output+f"speedy_{time_path}/{image_name}",speedy_images);image_name = f"data_augmente_{time.time()}.jpg"
 
-    
-    
-     
+    speedy_images= am.add_speed(image)
+
+    snowy_images= am.add_snow(image, snow_coeff=0.3)
+    image_name = f"data_augmente_{time.time()}.{type}"
+    if copy_txt:
+        txt_name = os.path.basename(img).replace(f".{type}",".txt")
+        txt_path_new = os.path.join(output, "snowy", txt_name)
+        txt_path_before = img.replace(f".{type}",".txt")
+        shutil.copy(txt_path_before,txt_path_new)
+    cv2.imwrite(os.path.join(output, "snowy", image_name), snowy_images)
+
+    rainy_images= am.add_rain(image, rain_type='heavy', slant=20)
+    image_name = f"data_augmente_{time.time()}.{type}"
+    if copy_txt:
+        txt_name = os.path.basename(img).replace(f".{type}",".txt")
+        txt_path_new = os.path.join(output, "rainy", txt_name)
+        txt_path_before = img.replace(f".{type}",".txt")
+        shutil.copy(txt_path_before,txt_path_new)
+    cv2.imwrite(os.path.join(output, "rainy", image_name), rainy_images)
+
+    cv2.imwrite(os.path.join(output, "bright", image_name), bright_image)
+    cv2.imwrite(os.path.join(output, "dark", image_name), dark_image)
+    cv2.imwrite(os.path.join(output, "shadowy", image_name), shadowy_images)
+    cv2.imwrite(os.path.join(output, "speedy", image_name), speedy_images)
